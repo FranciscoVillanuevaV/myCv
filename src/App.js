46a8dc766bf4872b60f0ee419bd7b5e8e5f3a64c
FranciscoVillanuevaV <villanuevavazquez.francisco@gmail.com>
2021-02-26 02:31:21 -0600

    refactor

--- src/App.js
+++ src/App.js
@@ -1,22 +1,105 @@
-import './App.css';
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import Header from './Components/Header';
-import Menu from './Components/Menu';
 import PersonalInfo from './Components/PersonalInfo';
 import Education from './Components/Education';
 import Experience from './Components/Experience';
 import Skills from './Components/Skills';
 
-function App() {
-  return (
-    <div className="container-fluid col-lg-9 col-md-11 col-sm-9">
-      <Header/>
-      <Menu/>
-      <PersonalInfo/>
-      <Education/>
-      <Experience/>
-      <Skills/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.header = React.createRef();
+    this.education = React.createRef();
+    this.experience = React.createRef();
+    this.skills = React.createRef();
+    this.menu = React.createRef();
+  }
+  componentDidMount() {
+    AOS.init();
+    this.stickFix();
+		window.addEventListener('resize', this.stickFix);
+  }
+  calculatePosition = reference => {
+    let topPos = 0;
+    if (reference.current)
+    {
+      topPos = reference.current.getBoundingClientRect().top;
+    }
+    return topPos + window.scrollY;
+  }
+  returnPosition = location => {
+    switch (location) {
+      case 'header':
+        return this.calculatePosition(this.header);
+      case 'education':
+        return this.calculatePosition(this.education);;
+      case 'skills':
+        return this.calculatePosition(this.skills);;
+      case 'experience':
+        return this.calculatePosition(this.experience);;
+      default:
+        return 0;
+    }
+  }
+  scrollToElement = location => {
+		const position = this.returnPosition(location);
+		window.scroll({
+			top: position - this.menuHeight,
+			left: 0,
+			behavior: 'smooth'
+		});
+	}
+  stickFix = () => {
+		if (this.menu.current) {
+			if ( window.innerWidth < 993 ) {
+				this.menu.current.classList.remove('sticky-top');
+				this.menuHeight = 0;
+			}else{
+				this.menu.current.classList.add('sticky-top');
+				this.menuHeight = 60;
+			}
+		}
+	}
+  render() {
+    return (
+      <div style={{fontFamily: 'Arial, sans-serif'}} className="container-fluid col-lg-9 col-md-11 col-sm-9">
+        <div ref={this.header} >
+          <Header/>
+        </div>
+        <Navbar ref={this.menu} expand="lg" bg="dark" variant="dark" className="rounded">
+          <Navbar.Brand onClick={() => this.scrollToElement('header')}>Curriculum Vitae</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link onClick={() => this.scrollToElement('education')}>Education</Nav.Link>
+              <Nav.Link onClick={() => this.scrollToElement('experience')}>Professional experience</Nav.Link>
+              <Nav.Link onClick={() => this.scrollToElement('skills')}>Skills</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="3000">
+          <PersonalInfo/>
+        </div>
+        <p ref={this.education}></p>
+        <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="3000">
+          <Education/>
+        </div>
+        <p ref={this.experience}></p>
+        <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="3000">
+          <Experience />
+        </div>
+        <p ref={this.skills}></p>
+        <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="3000">
+          <Skills/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
